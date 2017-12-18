8e0a028a2aa36d5c5f7446e2bd29905961c99785
Evgeniy Borisov <c_epbori@quicinc.com>
2017-12-18 13:53:55 +0200

    Initial commit

---
+++ project/simple-nrf51822-linux/main.c
@@ -0,0 +1,130 @@
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+#include "nrf_sdm.h"
+#include "ble.h"
+#include "ble_bas.h"
+#include "ble_hrs.h"
+
+
+#include "softdevice_handler.h"
+
+static ble_bas_t                         m_bas;                                     /**< Structure used to identify the battery service. */
+static ble_hrs_t                         m_hrs;                                     /**< Structure used to identify the heart rate service. */
+
+static uint16_t                          m_conn_handle = BLE_CONN_HANDLE_INVALID;   /**< Handle of the current connection. */
+
+
+#define BSP_INDICATE_CONNECTED 1
+
+/**@brief Function for handling the Application's BLE Stack events.
+ *
+ * @param[in] p_ble_evt  Bluetooth stack event.
+ */
+static void on_ble_evt(ble_evt_t * p_ble_evt)
+{
+    uint32_t err_code;
+
+    switch (p_ble_evt->header.evt_id)
+            {
+        case BLE_GAP_EVT_CONNECTED:
+            err_code = bsp_indication_set(BSP_INDICATE_CONNECTED);
+            APP_ERROR_CHECK(err_code);
+            m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
+            break;
+
+        case BLE_GAP_EVT_DISCONNECTED:
+            m_conn_handle = BLE_CONN_HANDLE_INVALID;
+            break;
+
+        default:
+            // No implementation needed.
+            break;
+    }
+}
+
+
+
+
+/**@brief Function for dispatching a BLE stack event to all modules with a BLE stack event handler.
+ *
+ * @details This function is called from the BLE Stack event interrupt handler after a BLE stack
+ *          event has been received.
+ *
+ * @param[in] p_ble_evt  Bluetooth stack event.
+ */
+static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
+{
+    dm_ble_evt_handler(p_ble_evt);
+    ble_hrs_on_ble_evt(&m_hrs, p_ble_evt);
+    ble_bas_on_ble_evt(&m_bas, p_ble_evt);
+    ble_conn_params_on_ble_evt(p_ble_evt);
+    bsp_btn_ble_on_ble_evt(p_ble_evt);
+#ifdef BLE_DFU_APP_SUPPORT
+    /** @snippet [Propagating BLE Stack events to DFU Service] */
+    ble_dfu_on_ble_evt(&m_dfus, p_ble_evt);
+    /** @snippet [Propagating BLE Stack events to DFU Service] */
+#endif // BLE_DFU_APP_SUPPORT
+    on_ble_evt(p_ble_evt);
+    ble_advertising_on_ble_evt(p_ble_evt);
+}
+
+/**@brief Function for dispatching a system event to interested modules.
+ *
+ * @details This function is called from the System event interrupt handler after a system
+ *          event has been received.
+ *
+ * @param[in] sys_evt  System stack event.
+ */
+static void sys_evt_dispatch(uint32_t sys_evt)
+{
+    pstorage_sys_event_handler(sys_evt);
+    ble_advertising_on_sys_evt(sys_evt);
+}
+
+
+/**@brief Function for initializing the BLE stack.
+ *
+ * @details Initializes the SoftDevice and the BLE event interrupt.
+ */
+static void ble_stack_init(void)
+{
+    uint32_t err_code;
+
+    // Initialize the SoftDevice handler module.
+    SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_20_PPM, NULL);
+
+#if defined(S110) || defined(S130) || defined(S310)  || defined(S132)
+    // Enable BLE stack.
+    ble_enable_params_t ble_enable_params;
+    memset(&ble_enable_params, 0, sizeof(ble_enable_params));
+#if defined(S130) || defined(S310) || defined(S132)
+    ble_enable_params.gatts_enable_params.attr_tab_size   = BLE_GATTS_ATTR_TAB_SIZE_DEFAULT;
+#endif
+    ble_enable_params.gatts_enable_params.service_changed = IS_SRVC_CHANGED_CHARACT_PRESENT;
+    err_code = sd_ble_enable(&ble_enable_params);
+    APP_ERROR_CHECK(err_code);
+#endif
+
+    // Register with the SoftDevice handler module for BLE events.
+    err_code = softdevice_ble_evt_handler_set(ble_evt_dispatch);
+    APP_ERROR_CHECK(err_code);
+
+    // Register with the SoftDevice handler module for BLE events.
+    err_code = softdevice_sys_evt_handler_set(sys_evt_dispatch);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+int main(void){
+
+    ble_stack_init();
+
+	nrf_gpio_range_cfg_output(18,19);
+	while(1){
+		nrf_gpio_port_write(2,8);
+		nrf_delay_ms(80);
+		nrf_gpio_port_write(2,4);
+		nrf_delay_ms(80);
+	}
+
+}
